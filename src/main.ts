27d7ad8242510c7ad49949f4bf7fcbc2a67083eb
wiphy <lm802085@gmail.com>
2026-02-16 18:48:32 +0100

    fix: update CORS configuration to use dynamic frontend URL

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  if (process.env.NODE_ENV === 'development') {
-    app.enableCors();
-  }
+  app.enableCors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
